0ed53ed1d31816989939363fd02f4526b90b2c77
Chukzy11 <Ikechukz20@gmail.com>
2024-11-16 09:23:16 +0100

    updated the http request

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,20 +11,37 @@ func main() {
 
 	// http.HandleFunc("/", handler)
 	// http.HandleFunc("/root", root)
-	//
+	
 	// log.Println("server running on localhost:8000")
-	// http.ListenAndServe(":8000", nil)
+	// err := http.ListenAndServe(":8000", nil)
+	// if err != nil {
+	// 	log.Println("error in listen and serve: ", err)
+	// 	return
+	// }
 
 	router := gin.Default()
 
 	router.GET("/", home)
+	router.POST("/post-sample", examplePost)
+	router.PATCH("/patch-sample", examplePatch)
 	log.Println("server running on localhost:8000")
+
+	srv := &http.Server{
+		Addr: ":8000",
+		Handler: router,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Println("error in listen and serve: ", err)
+		return
+	}
 }
 
 //http packages: these are used to establish a connection between a client (web browser) and a server (your base)
 //DIFFERENT HTTP METHODS: GET, POST, PUT, PATCH, DELETE.
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request){
 	if r.Method == "GET" {
 		fmt.Fprintf(w, "hello welcome to our web page")
 		return
@@ -45,11 +61,45 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "we are only processing GET and POST requests")
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
+func root(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "this is the root url: %s", r.URL.Path[1:])
 }
 
 func home(c *gin.Context) {
 	c.JSON(http.StatusOK, "welcome to my home page")
-	// testing new branch
+}
+
+type postRequest struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
+
+func examplePost(c *gin.Context){
+	req := postRequest{}
+
+	err:= c.ShouldBindBodyWithJSON(&req)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+	// create this in the database
+
+	ans := fmt.Sprintf("%s is %v years old: ", req.Name, req.Age)
+
+	c.JSON(http.StatusOK, ans)
+}
+
+func examplePatch(c *gin.Context){
+	req := postRequest{}
+
+	err:= c.ShouldBindBodyWithJSON(&req)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+	// change or update something in the database
+
+	ans := fmt.Sprintf("%s is %v years old: ", req.Name, req.Age)
+
+	c.JSON(http.StatusOK, ans)
 }
